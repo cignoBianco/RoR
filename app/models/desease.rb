d44fa64bd7456760beb13488262ea71116b29e41
cignobianco <smiana123@gmail.com>
2021-01-08 15:34:50 +0700

    dependant destroy

--- app/models/desease.rb
+++ app/models/desease.rb
@@ -1,6 +1,6 @@
 class Desease < ApplicationRecord
-  has_many :symptoms_of_desease
-  has_many :symptoms, through: :symptoms_of_desease
+  has_many :symptoms_of_desease, dependent: :destroy
+  has_many :symptoms, through: :symptoms_of_desease, dependent: :destroy
   
 
 end
--- app/models/symptom.rb
+++ app/models/symptom.rb
@@ -1,6 +1,6 @@
 class Symptom < ApplicationRecord
 
-  has_many :symptoms_of_desease
-  has_many :deseases, through: :symptoms_of_desease
+  has_many :symptoms_of_desease, dependent: :destroy
+  has_many :deseases, through: :symptoms_of_desease, dependent: :destroy
 
 end
