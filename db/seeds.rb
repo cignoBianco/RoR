8710e42e9d62ac28a03ed2657910ce86fba30f2e
cignobianco <smiana123@gmail.com>
2021-01-08 14:59:36 +0700

    seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,12 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 #
-#deseases = Desease.create([{ title: ''},])
+deseases = Desease.create([{ title: 'Desease 1', description: 'Description 1', body_part: 'HEAD'},{title: 'Desease 2', description: 'description 2', body_part: 'ARM'}, {title: 'Desease 3', description: 'Description 3', body_part: 'HEAD'}, {title: 'Desease 4', description: 'Description 4', body_part: 'Body_part 4'}])
+
+symptoms = Symptom.create([{title: 'Symptom 1', description: 'Description 1'}, {title: 'Symptom 2', description: 'Symptoms description 2'}])
+
+desease_symptoms = Symptoms_of_deseases.create([{desease_id: 1, symptom_id: 1},
+  {desease_id: 1, symptom_id: 2},
+  {desease_id: 2, symptom_id: 3},
+  {desease_id: 3, symptom_id: 3},
+  {desease_id: 3, symptom_id: 1}])
